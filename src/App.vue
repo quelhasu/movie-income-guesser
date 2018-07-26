37660a20dc711fe4ef42398a7a08306d41f596d6
Ugo Quelhas <uquelhas@gmail.com>
2018-07-26 22:07:40 +0200

    :children_crossing: css improve

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
   <div id="app">
+    <a href="https://github.com/quelhasu/movie-income-guesser" target="blank_" class="github-corner" aria-label="View source on Github">
+  <svg id="github" width="80" height="80" viewBox="0 0 250 250" style="fill:#fff; position: absolute; top: 0; border: 0; right: 0;"
+    aria-hidden="true">
+    <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
+    <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
+      fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
+    <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"
+      fill="currentColor" class="octo-body"></path>
+  </svg>
+</a>
     <div class="header">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-      <h1>Who's making the highest income ?</h1>
+      <!-- <h1>WHO'S MAKING THE <span id="mainger">HIGHEST</span> INCOME ?</h1> -->
+      <vue-typer 
+      :text="typerText" 
+      :repeat="typerRepeat" 
+      :shuffle='true'
+      :pre-type-delay='1500' 
+      :type-delay='50' 
+      :pre-erase-delay='5000'
+      :erase-delay='75' 
+      :erase-on-complete="typerErase" 
+      erase-style='select-back' 
+      caret-animation='smooth'
+    ></vue-typer>
+
     </div>
     <div class="cards">
       <div v-for="(card, index) in cards" class="card" :ref="'card'+index" @click="verify('card'+index, index)" :key="index">
@@ -56,6 +79,10 @@
 import axios from "axios";
 import ICountUp from "vue-countup-v2";
 import moment from "moment";
+import VueTyperPlugin from "vue-typer";
+import Vue from "vue";
+
+Vue.use(VueTyperPlugin);
 
 export default {
   name: "app",
@@ -81,11 +108,26 @@ export default {
       firstC: {},
       secondC: {},
       verified: false,
-      waitingTime: 3,
+      waitingTime: 2,
       most: "",
       winner: "",
       wins: 0,
-      looses: 0
+      looses: 0,
+      typerText: ["WHO'S MAKING THE HIGHEST INCOME?"],
+      typerArray: [
+        "Really?",
+        "Oh, you're still here?",
+        "How are you?",
+        "Not great movies, huh..?",
+        "???",
+        "Don't you get pissed off?",
+        "So?",
+        "I'll be back, just a minute..",
+        "You're not very good..",
+        "Oh you can check my GitHub"
+      ],
+      typerErase: false,
+      typerRepeat: "0"
     };
   },
   filters: {
@@ -134,6 +176,14 @@ export default {
         .catch(e => {
           this.errors.push(e);
         });
+    },
+    changeTypeText() {
+      var newText = this.typerArray[
+        Math.floor(Math.random() * this.typerArray.length)
+      ];
+      this.typerText = this.typerArray;
+      this.typerErase = true;
+      this.typerRepeat = "Infinity";
     },
     defaultImage(img) {
       this.$refs[img][0].src =
@@ -231,6 +281,9 @@ export default {
           vm.updateCard(vm);
         }, (this.waitingTime + 1) * 1000);
         setTimeout(function() {
+          if (!this.typerErase) vm.changeTypeText();
+        }, (this.waitingTime + 2) * 1000);
+        setTimeout(function() {
           vm.selectMovies();
           card === "card" + vm.winner ? vm.wins++ : vm.looses++;
         }, (this.waitingTime + 3) * 1000);
@@ -281,7 +334,7 @@ export default {
   box-sizing: border-box;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
